e49e628b1d3bebde8da08374fa1c919504f1a496
Gabor Szarnyas <szarnyasg@gmail.com>
2020-12-14 01:21:15 +0100

    duckdb

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
 if [[ ! -z $(which dnf) ]]; then
     sudo dnf install -y pv
 elif [[ ! -z $(which apt) ]]; then
     sudo apt install -y pv
 fi
+
+DUCKDB_VERSION=v0.2.2
+
+cd duc-scratch
+wget https://github.com/cwida/duckdb/releases/download/${DUCKDB_VERSION}/duckdb_cli-linux-amd64.zip
+unzip duckdb_cli-linux-amd64.zip
+rm duckdb_cli-linux-amd64.zip
--- scripts/load-duc.sh
+++ scripts/load-duc.sh
@@ -5,7 +5,8 @@ cd ..
 
 IMPORT_DATA_DIR=`pwd`/data/social_network_preprocessed
 DUCKDB_DIR=`pwd`/duc-scratch
+DUCKDB_BINARY=`pwd`/duc-scratch/duckdb
 
 rm ${DUCKDB_DIR}/ldbc.duckdb*
-cat duc/schema.sql | duckdb ${DUCKDB_DIR}/ldbc.duckdb
-sed "s|PATHVAR|${IMPORT_DATA_DIR}|" duc/snb-load.sql | duckdb ${DUCKDB_DIR}/ldbc.duckdb
+cat duc/schema.sql | ${DUCKDB_BINARY} ${DUCKDB_DIR}/ldbc.duckdb
+sed "s|PATHVAR|${IMPORT_DATA_DIR}|" duc/snb-load.sql | ${DUCKDB_BINARY} ${DUCKDB_DIR}/ldbc.duckdb
