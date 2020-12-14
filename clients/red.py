e28d8cbd79939cea3c3b80046ddd51abdc65fa15
Gabor Szarnyas <szarnyasg@gmail.com>
2020-12-14 12:06:33 +0100

    Fix RG query

--- clients/red.py
+++ clients/red.py
@@ -3,6 +3,6 @@ from redisgraph import Node, Edge, Graph, Path
 
 redis = redis.Redis(host='localhost', port=6379)
 graph = Graph('SocialGraph', redis)
-query = """MATCH (p1:Person)-[:KNOWS]-(p2:Person)-[:KNOWS]-(p3:Person)-[:KNOWS]-(p1:Person) RETURN count(*) AS triangleCount"""
+query = """MATCH p=(p1:Person)-[:KNOWS]-(p2:Person)-[:KNOWS]-(p3:Person)-[:KNOWS]-(p1:Person) RETURN count(p) AS triangleCount"""
 result = graph.query(query)
 result.pretty_print()
