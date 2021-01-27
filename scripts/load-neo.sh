d78b176c49deaef5622c662e3f1ac09d1bb76a9b
Gabor Szarnyas <szarnyasg@gmail.com>
2021-01-27 21:20:34 +0100

    Use more data

--- scripts/load-neo.sh
+++ scripts/load-neo.sh
@@ -6,6 +6,9 @@ cd ..
 . scripts/neo-vars.sh
 . scripts/import-vars.sh
 
+# make sure Neo4j is stopped
+scripts/stop-neo4j.sh
+
 # make sure directories exist
 mkdir -p ${NEO4J_HOME}/{logs,import,plugins}
 
@@ -22,14 +25,35 @@ docker run --rm \
     ${NEO4J_ENV_VARS} \
     neo4j:${NEO4J_VERSION} \
     neo4j-admin import \
-    --nodes=Forum=/import/forum.csv \
-    --nodes=Message=/import/message.csv \
-    --nodes=Person=/import/person.csv \
-    --nodes=Tag=/import/tag.csv \
-    --relationships=FORUM_HAS_TAG=/import/forum_hasTag_tag.csv \
-    --relationships=HAS_INTEREST=/import/person_hasInterest_tag.csv \
-    --relationships=HAS_MEMBER=/import/forum_hasMember_person.csv \
-    --relationships=KNOWS=/import/person_knows_person.csv \
-    --relationships=LIKES=/import/person_likes_message.csv \
-    --relationships=MESSAGE_HAS_TAG=/import/message_hasTag_tag.csv \
+    --nodes=Place="/import/place.csv" \
+    --nodes=Organisation="/import/organisation.csv" \
+    --nodes=TagClass="/import/tagclass.csv" \
+    --nodes=Tag="/import/tag.csv" \
+    --nodes=Forum="/import/forum.csv" \
+    --nodes=Person="/import/person.csv" \
+    --nodes=Message:Comment="/import/comment.csv" \
+    --nodes=Message:Post="/import/post.csv" \
+    --relationships=IS_PART_OF="/import/place_isPartOf_place.csv" \
+    --relationships=IS_SUBCLASS_OF="/import/tagclass_isSubclassOf_tagclass.csv" \
+    --relationships=IS_LOCATED_IN="/import/organisation_isLocatedIn_place.csv" \
+    --relationships=HAS_TYPE="/import/tag_hasType_tagclass.csv" \
+    --relationships=HAS_CREATOR="/import/comment_hasCreator_person.csv" \
+    --relationships=IS_LOCATED_IN="/import/comment_isLocatedIn_place.csv" \
+    --relationships=REPLY_OF="/import/comment_replyOf_comment.csv" \
+    --relationships=REPLY_OF="/import/comment_replyOf_post.csv" \
+    --relationships=CONTAINER_OF="/import/forum_containerOf_post.csv" \
+    --relationships=HAS_MEMBER="/import/forum_hasMember_person.csv" \
+    --relationships=HAS_MODERATOR="/import/forum_hasModerator_person.csv" \
+    --relationships=HAS_TAG="/import/forum_hasTag_tag.csv" \
+    --relationships=HAS_INTEREST="/import/person_hasInterest_tag.csv" \
+    --relationships=IS_LOCATED_IN="/import/person_isLocatedIn_place.csv" \
+    --relationships=KNOWS="/import/person_knows_person.csv" \
+    --relationships=LIKES="/import/person_likes_comment.csv" \
+    --relationships=LIKES="/import/person_likes_post.csv" \
+    --relationships=HAS_CREATOR="/import/post_hasCreator_person.csv" \
+    --relationships=HAS_TAG="/import/comment_hasTag_tag.csv" \
+    --relationships=HAS_TAG="/import/post_hasTag_tag.csv" \
+    --relationships=IS_LOCATED_IN="/import/post_isLocatedIn_place.csv" \
+    --relationships=STUDY_AT="/import/person_studyAt_organisation.csv" \
+    --relationships=WORK_AT="/import/person_workAt_organisation.csv" \
     --delimiter '|'
--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-# script to preprocess CSV files produced by the LDBC SNB Datagen (CsvBasic or CsvComposite serializers).
-# the expected output files are the following:
-#
-# nodes:
-# - forum.csv
-# - message.csv
-# - person.csv
-# - tag.csv
-#
-# edges:
-# - forum_hasMember_person.csv
-# - forum_hasTag_tag.csv
-# - message_hasTag_tag.csv
-# - person_hasInterest_tag.csv
-# - person_knows_person.csv
-# - person_likes_message.csv
+# script to preprocess CSV files produced by the LDBC SNB Datagen (CsvSingularProjectedFK or CsvSingularMergedFK serializers).
 
 RAW_DATA_DIR=${1:-'data/social_network'}
 
@@ -24,14 +9,35 @@ cd ..
 
 OUTPUT_DATA_DIR=`pwd`/data/social_network_preprocessed
 
+# make sure the output directory exists
+mkdir -p ${OUTPUT_DATA_DIR}
+
 # static entities
-tail -n +2 ${RAW_DATA_DIR}/static/tag_0_0.csv | cut -d '|' -f 1 > ${OUTPUT_DATA_DIR}/tag.csv
+
+## nodes (the id is in column 1)
+echo "-------------------- static nodes --------------------"
+pv ${RAW_DATA_DIR}/static/tag_0_0.csv          | tail -n +2 | cut -d '|' -f 1   > ${OUTPUT_DATA_DIR}/tag.csv
+pv ${RAW_DATA_DIR}/static/tagclass_0_0.csv     | tail -n +2 | cut -d '|' -f 1   > ${OUTPUT_DATA_DIR}/tagclass.csv
+pv ${RAW_DATA_DIR}/static/place_0_0.csv        | tail -n +2 | cut -d '|' -f 1,4 > ${OUTPUT_DATA_DIR}/place.csv
+pv ${RAW_DATA_DIR}/static/organisation_0_0.csv | tail -n +2 | cut -d '|' -f 1,2 > ${OUTPUT_DATA_DIR}/organisation.csv
+
+## edges (the source and target ids are in columns 1 and 2)
+echo "-------------------- static edges --------------------"
+for entity in \
+    organisation_isLocatedIn_place \
+    place_isPartOf_place \
+    tag_hasType_tagclass \
+    tagclass_isSubclassOf_tagclass \
+    ; \
+do
+    pv ${RAW_DATA_DIR}/static/${entity}_0_0.csv | tail -n +2 | cut -d '|' -f 1,2 > ${OUTPUT_DATA_DIR}/${entity}.csv
+done
 
 # dynamic entities
 ## these start with a creation date which should be omitted
 
 ## nodes (the id is in column 2)
-echo "nodes"
+echo "-------------------- dynamic nodes -------------------"
 for entity in \
     comment \
     forum \
@@ -43,34 +49,45 @@ do
 done
 
 ## edges (the source and target ids are in columns 2 and 3)
-echo "edges"
+echo "-------------------- dynamic edges -------------------"
 for entity in \
+    comment_hasCreator_person \
     comment_hasTag_tag \
+    comment_isLocatedIn_place \
+    comment_replyOf_comment \
+    comment_replyOf_post \
+    forum_containerOf_post \
     forum_hasMember_person \
+    forum_hasModerator_person \
     forum_hasTag_tag \
     person_hasInterest_tag \
+    person_isLocatedIn_place \
     person_knows_person \
     person_likes_comment \
     person_likes_post \
+    person_studyAt_organisation \
+    person_workAt_organisation \
+    post_hasCreator_person \
     post_hasTag_tag \
+    post_isLocatedIn_place \
     ; \
 do
     pv ${RAW_DATA_DIR}/dynamic/${entity}_0_0.csv | tail -n +2 | cut -d '|' -f 2,3 > ${OUTPUT_DATA_DIR}/${entity}.csv
 done
 
-## merge posts and comments to message
-echo "merging messages"
-pv ${OUTPUT_DATA_DIR}/{comment,post}.csv > ${OUTPUT_DATA_DIR}/message.csv
-pv ${OUTPUT_DATA_DIR}/person_likes_{comment,post}.csv > ${OUTPUT_DATA_DIR}/person_likes_message.csv
-pv ${OUTPUT_DATA_DIR}/{comment,post}_hasTag_tag.csv > ${OUTPUT_DATA_DIR}/message_hasTag_tag.csv
+# ## merge posts and comments to message
+# echo "merging messages"
+# pv ${OUTPUT_DATA_DIR}/{comment,post}.csv > ${OUTPUT_DATA_DIR}/message.csv
+# pv ${OUTPUT_DATA_DIR}/person_likes_{comment,post}.csv > ${OUTPUT_DATA_DIR}/person_likes_message.csv
+# pv ${OUTPUT_DATA_DIR}/{comment,post}_hasTag_tag.csv > ${OUTPUT_DATA_DIR}/message_hasTag_tag.csv
 
-## cleanup
-rm ${OUTPUT_DATA_DIR}/{comment,post}.csv
-rm ${OUTPUT_DATA_DIR}/person_likes_{comment,post}.csv
-rm ${OUTPUT_DATA_DIR}/{comment,post}_hasTag_tag.csv
+# ## cleanup
+# rm ${OUTPUT_DATA_DIR}/{comment,post}.csv
+# rm ${OUTPUT_DATA_DIR}/person_likes_{comment,post}.csv
+# rm ${OUTPUT_DATA_DIR}/{comment,post}_hasTag_tag.csv
 
 # add headers
-echo "add headers"
+echo "--------------------- add headers --------------------"
 while read line; do
   IFS=' ' read -r -a array <<< $line
   FILENAME=${array[0]}
