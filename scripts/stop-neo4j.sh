41d77479e85266342e88eaef11ed169a51f07f53
Gabor Szarnyas <szarnyasg@gmail.com>
2021-01-27 19:22:46 +0100

    Add script to stop Neo4j database

---
+++ scripts/stop-neo4j.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+. scripts/neo-vars.sh
+
+docker stop ${NEO4J_CONTAINER_NAME} || echo "No container ${NEO4J_CONTAINER_NAME} found"
