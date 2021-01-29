041a4506c4e57eb0609b555c46b4191258518c56
Gabor Szarnyas <szarnyasg@gmail.com>
2021-01-29 19:39:41 +0100

    Force delete in load script

--- scripts/load-ddb.sh
+++ scripts/load-ddb.sh
@@ -9,6 +9,6 @@ IMPORT_DATA_DIR=`pwd`/data/social_network_preprocessed
 DUCKDB_DIR=`pwd`/ddb-scratch
 DUCKDB_BINARY=`pwd`/ddb-scratch/duckdb
 
-rm ${DUCKDB_DIR}/ldbc.duckdb*
+rm -f ${DUCKDB_DIR}/ldbc.duckdb*
 cat ddb/schema.sql | ${DUCKDB_BINARY} ${DUCKDB_DIR}/ldbc.duckdb
 sed "s|PATHVAR|${IMPORT_DATA_DIR}|" ddb/snb-load.sql | ${DUCKDB_BINARY} ${DUCKDB_DIR}/ldbc.duckdb
