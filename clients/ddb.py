f2b8f360105a87fce727d8bbf50310a22d3af352
Gabor Szarnyas <szarnyasg@gmail.com>
2021-01-29 16:29:24 +0100

    Attr name

--- clients/ddb.py
+++ clients/ddb.py
@@ -4,9 +4,9 @@ con = duckdb.connect(database='ddb-scratch/ldbc.duckdb', read_only=True)
 con.execute("""
 SELECT count(*)
 FROM person_knows_person k1, person_knows_person k2, person_knows_person k3
-WHERE k1.person2_id = k2.person1_id
-  AND k2.person2_id = k3.person1_id
-  AND k3.person2_id = k1.person1_id;
+WHERE k1.person2id = k2.person1id
+  AND k2.person2id = k3.person1id
+  AND k3.person2id = k1.person1id;
 """)
 
 print(con.fetchall())
