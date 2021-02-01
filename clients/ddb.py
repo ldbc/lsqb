7ce6f409a1c7aa84f9c19345475651ff09f64936
Gabor Szarnyas <szarnyasg@gmail.com>
2021-02-01 15:32:50 +0100

    Format queries

--- clients/ddb.py
+++ clients/ddb.py
@@ -12,28 +12,30 @@ def run_query(con, query_id, query_spec):
 
 con = duckdb.connect(database='ddb-scratch/ldbc.duckdb', read_only=True)
 
+# using FROM + WHERE
 run_query(con, "3a", """
   SELECT count(*)
-  FROM message_hasTag_tag, comment_replyOf_message, comment_hasTag_tag AS comment_hasTag_tag1
+  FROM message_hasTag_tag, comment_replyOf_message, comment_hasTag_tag AS cht1
   WHERE message_hasTag_tag.MessageId = comment_replyOf_message.ParentMessageId
-    AND comment_replyOf_message.CommentId = comment_hasTag_tag1.CommentId
-    AND message_hasTag_tag.TagId != comment_hasTag_tag1.TagId
-    AND NOT EXISTS (SELECT 1 FROM comment_hasTag_tag AS comment_hasTag_tag2
-      WHERE message_hasTag_tag.TagId = comment_hasTag_tag2.TagId
-        AND comment_replyOf_message.CommentId = comment_hasTag_tag2.CommentId
+    AND comment_replyOf_message.CommentId = cht1.CommentId
+    AND message_hasTag_tag.TagId != cht1.TagId
+    AND NOT EXISTS (SELECT 1 FROM comment_hasTag_tag AS cht2
+      WHERE message_hasTag_tag.TagId = cht2.TagId
+        AND comment_replyOf_message.CommentId = cht2.CommentId
     );
   """)
 
+# using JOIN ON
 run_query(con, "3b", """
   SELECT count(*)
   FROM message_hasTag_tag
   JOIN comment_replyOf_message
     ON message_hasTag_tag.MessageId = comment_replyOf_message.ParentMessageId
-  JOIN comment_hasTag_tag
-    ON comment_replyOf_message.CommentId = comment_hasTag_tag.CommentId
-  LEFT JOIN comment_hasTag_tag AS cht
-        ON message_hasTag_tag.TagId = cht.TagId
-        AND comment_replyOf_message.CommentId = cht.CommentId
-  WHERE message_hasTag_tag.TagId != comment_hasTag_tag.TagId
-    AND cht.TagId IS NULL;
+  JOIN comment_hasTag_tag AS cht1
+    ON comment_replyOf_message.CommentId = cht1.CommentId
+  LEFT JOIN comment_hasTag_tag AS cht2
+         ON message_hasTag_tag.TagId = cht2.TagId
+        AND comment_replyOf_message.CommentId = cht2.CommentId
+  WHERE message_hasTag_tag.TagId != cht1.TagId
+    AND cht2.TagId IS NULL;
   """)
