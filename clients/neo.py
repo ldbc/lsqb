4ac94c083c725ff51d7ab154465dc3ebc05e2c03
Gabor Szarnyas <szarnyasg@gmail.com>
2021-02-03 13:36:15 +0100

    Fix variable name in Q5/Cypher implementation

--- clients/neo.py
+++ clients/neo.py
@@ -44,7 +44,7 @@ with driver.session() as session:
         MATCH (pA:Person)-[:IS_LOCATED_IN]->(:City)-[:IS_PART_OF]->(country)
         MATCH (pB:Person)-[:IS_LOCATED_IN]->(:City)-[:IS_PART_OF]->(country)
         MATCH (pC:Person)-[:IS_LOCATED_IN]->(:City)-[:IS_PART_OF]->(country)
-        MATCH (pA)-[k1:KNOWS]-(pB)-[k2:KNOWS]-(pC)-[k3:KNOWS]-(pa)
+        MATCH (pA)-[k1:KNOWS]-(pB)-[k2:KNOWS]-(pC)-[k3:KNOWS]-(pA)
         RETURN count(*) AS count
         """)
     run_query(session, 6, """
