313a47fd9caa61dc04bdb0a56b737e45de4920fe
Gabor Szarnyas <szarnyasg@gmail.com>
2021-02-09 15:41:48 +0100

    Fix indentation

--- sql/q3.sql
+++ sql/q3.sql
@@ -7,5 +7,5 @@ JOIN comment_hasTag_tag AS cht1
 LEFT JOIN comment_hasTag_tag AS cht2
        ON message_hasTag_tag.TagId = cht2.TagId
       AND comment_replyOf_message.CommentId = cht2.CommentId
-WHERE message_hasTag_tag.TagId != cht1.TagId
-  AND cht2.TagId IS NULL;
+    WHERE message_hasTag_tag.TagId != cht1.TagId
+      AND cht2.TagId IS NULL;
