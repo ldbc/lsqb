b82fda86774ed413138fd3de9bb329b5ee2c74ac
Gabor Szarnyas <szarnyasg@gmail.com>
2021-02-09 15:42:05 +0100

    Optimize Q6: use left join instead of subquery

--- sql/q6.sql
+++ sql/q6.sql
@@ -1,8 +1,11 @@
 SELECT count(*)
-FROM person_knows_person pkp1, person_knows_person pkp2, person_hasInterest_tag
-WHERE pkp1.Person2Id = pkp2.Person1Id
-  AND pkp2.Person2Id = person_hasInterest_tag.PersonId
-  AND pkp1.Person1Id != pkp2.Person2Id
-  AND NOT EXISTS (SELECT 1 from person_knows_person pkp3
-    WHERE pkp3.Person2Id = pkp2.Person2Id 
-      AND pkp3.Person1Id = pkp1.Person1Id);
+FROM person_knows_person pkp1
+JOIN person_knows_person pkp2
+  ON pkp1.Person2Id = pkp2.Person1Id
+ AND pkp1.Person1Id != pkp2.Person2Id
+JOIN person_hasInterest_tag
+  ON pkp2.Person2Id = person_hasInterest_tag.PersonId
+LEFT JOIN person_knows_person pkp3
+       ON pkp3.Person1Id = pkp1.Person1Id
+      AND pkp3.Person2Id = pkp2.Person2Id
+    WHERE pkp3.Person1Id IS NULL;
