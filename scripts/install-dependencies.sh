6e5f55f2c0ab76d2660eb28fc996f5878e4535b3
Gabor Szarnyas <szarnyasg@gmail.com>
2021-02-16 18:15:57 +0100

    Add plotting dependencies

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -8,9 +8,14 @@ cd ..
 
 . ddb/vars.sh
 
+# clients
 pip3 install --user duckdb==${DUCKDB_VERSION}
 pip3 install --user neo4j
 pip3 install --user redisgraph redisgraph-bulk-loader
 pip3 install --user psycopg2-binary
 
+# DuckDB binary
 ddb/get.sh
+
+# misc
+pip3 install --user matplotlib pandas
