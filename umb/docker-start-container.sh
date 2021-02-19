a89874ed7ce58f6331bab2c817153074a2e86c2c
Gabor Szarnyas <szarnyasg@gmail.com>
2021-02-19 19:38:02 +0100

    Dockerize Umbra #12

--- umb/docker-start-container.sh
+++ umb/docker-start-container.sh
@@ -7,11 +7,10 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 . docker-vars.sh
 
-docker run -it \
+docker run \
     --publish=5432:5432 \
     --volume=${UMBRA_DATA_DIR}:/data \
     --volume=${UMBRA_SCRATCH_DIR}:/scratch \
     --name ${UMBRA_CONTAINER_NAME} \
     --detach \
-    ${UMBRA_DOCKER_IMAGE}:latest \
-    /bin/bash
+    ${UMBRA_DOCKER_IMAGE}:latest
