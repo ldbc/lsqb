2833e1312776ad86043d0fe98e26f7638d8b64cd
Gabor Szarnyas <szarnyasg@gmail.com>
2021-02-19 19:38:56 +0100

    Drop verbose mode

--- umb/docker-start-db.sh
+++ umb/docker-start-db.sh
@@ -7,5 +7,5 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 . docker-vars.sh
 
-docker exec ${UMBRA_CONTAINER_NAME} /umbra/bin/server -verbose -address 0.0.0.0 /scratch/ldbc.db
+docker exec ${UMBRA_CONTAINER_NAME} /umbra/bin/server -address 0.0.0.0 /scratch/ldbc.db
 sleep 1
