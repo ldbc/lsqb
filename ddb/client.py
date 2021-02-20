0e7a764ce2c8716b649497b0640bbd6d8f6cc5e1
Gabor Szarnyas <szarnyasg@gmail.com>
2021-02-20 22:30:46 +0100

    Add SF support to DuckDB implementation

--- ddb/client.py
+++ ddb/client.py
@@ -1,17 +1,24 @@
 import duckdb
 import time
+import sys
 
-def run_query(con, query_id, query_spec, numThreads):
+def run_query(con, sf, query_id, query_spec, numThreads):
     start = time.time()
     con.execute(f"PRAGMA threads={numThreads};\n" + query_spec)
     result = con.fetchall()
     end = time.time()
     duration = end - start
-    print(f"DuckDB\t{numThreads}\t{query_id}\t{duration:.4f}\t{result[0][0]}")
+    print(f"DuckDB\t{numThreads}\t{sf}\t{query_id}\t{duration:.4f}\t{result[0][0]}")
     return (duration, result)
 
+if len(sys.argv) < 2:
+  print("Usage: client.py sfX")
+  print("where X is the scale factor")
+  exit(1)
+
 con = duckdb.connect(database='ddb/scratch/ldbc.duckdb', read_only=True)
+sf = sys.argv[1]
 
 for i in range(1, 7):
   with open(f"sql/q{i}.sql", "r") as f:
-    run_query(con, i, f.read(), 4)
+    run_query(con, sf, i, f.read(), 4)
--- ddb/run.sh
+++ ddb/run.sh
@@ -7,5 +7,6 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
 . ddb/vars.sh
+. scripts/import-vars.sh
 
-python3 ddb/client.py
+python3 ddb/client.py ${SF}
