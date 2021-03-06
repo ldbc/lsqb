3800e60916b4ddec2c8a3515a6f7e8ba7a50d7b3
Gabor Szarnyas <szarnyasg@gmail.com>
2021-03-06 12:46:47 +0100

    Shuffle deps file

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -14,6 +14,9 @@ elif [[ ! -z $(which apt) ]]; then
     sudo apt install -y python3-pip zstd unzip
 fi
 
+# DuckDB binary
+ddb/get.sh
+
 # clients
 pip3 install --user duckdb==${DUCKDB_VERSION}
 pip3 install --user neo4j
@@ -21,8 +24,5 @@ pip3 install --user redisgraph redisgraph-bulk-loader
 pip3 install --user psycopg2-binary
 pip3 install --user mysql-connector-python
 
-# DuckDB binary
-ddb/get.sh
-
-# misc
+# visualization
 pip3 install --user matplotlib pandas seaborn natsort
