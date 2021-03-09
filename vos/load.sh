babb68b3dd8d2b30647e1c73ff7119bf55258299
Gabor Szarnyas <szarnyasg@gmail.com>
2021-03-09 19:17:37 +0100

    Remove comment

--- vos/load.sh
+++ vos/load.sh
@@ -9,7 +9,3 @@ cd ..
 . vos/vars.sh
 
 docker exec -it  ${VIRTUOSO_CONTAINER_NAME} isql-U ${VIRTUOSO_USER} -P ${VIRTUOSO_PWD} exec="LOAD /tmp/load.isql ;"
-
-
-# TODO
-# Refer to the script at https://github.com/ldbc/ldbc_snb_implementations/blob/fe82499dcc86f7e016475de17c55bd4776f8c2af/interactive/virtuoso/scripts/ld.sql
