375d0cc052a256c3f047727ec9d772f36c20dcaa
Gabor Szarnyas <szarnyasg@gmail.com>
2021-03-10 13:51:33 +0100

    Add check to Virtuoso load script to prevent loading nothing

--- vos/load.sh
+++ vos/load.sh
@@ -7,5 +7,11 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ..
 
 . vos/vars.sh
+. scripts/import-vars.sh
+
+if [ ! -f ${IMPORT_DATA_DIR_PROJECTED_FK}/ldbc.nt ]; then
+    echo "Input file ${IMPORT_DATA_DIR_PROJECTED_FK}/ldbc.nt does not exist"
+    exit 1
+fi
 
 docker exec -it ${VIRTUOSO_CONTAINER_NAME} isql -U ${VIRTUOSO_USER} -P ${VIRTUOSO_PWD} exec="LOAD /tmp/load.isql ;"
