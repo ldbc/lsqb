501eaa842cba49a1f7cccdfd5e203f33294f5a26
Gabor Szarnyas <szarnyasg@gmail.com>
2021-03-12 22:12:49 +0100

    Add Country to Q1

--- sql/q1.sql
+++ sql/q1.sql
@@ -4,8 +4,10 @@ FROM
   Forum,
   Comment,
   Post,
-  Comment_hasTag_Tag
+  Comment_hasTag_Tag,
+  City
 WHERE Forum.hasModerator_Person = Person.id
   AND Post.Forum_containerOf = Forum.id
   AND Post.id = Comment.replyOf_Post
-  AND Comment.id = Comment_hasTag_Tag.id;
+  AND Comment.id = Comment_hasTag_Tag.id
+  AND Person.isLocatedIn_Place = City.id;
