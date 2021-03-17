f69154a8482273d743f9d64f547a38eb49015325
Gabor Szarnyas <szarnyasg@gmail.com>
2021-03-17 22:14:24 +0100

    Add tag to Q1

--- sql/q1.sql
+++ sql/q1.sql
@@ -5,9 +5,12 @@ FROM
   Comment,
   Post,
   Comment_hasTag_Tag,
+  Tag,
   City
 WHERE Forum.hasModerator_Person = Person.id
   AND Post.Forum_containerOf = Forum.id
   AND Post.id = Comment.replyOf_Post
   AND Comment.id = Comment_hasTag_Tag.id
-  AND Person.isLocatedIn_City = City.id;
+  AND Person.isLocatedIn_City = City.id
+  AND Comment_hasTag_Tag.hasTag_Tag = Tag.id
+  ;
