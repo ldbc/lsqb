48e1492c94d46a01801b7830219481e2047cb6bd
Laurens Kuiper <laurens.kuiper@cwi.nl>
2021-03-22 14:51:34 +0100

    HyPer

---
+++ hyp/load.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+. hyp/vars.sh
+
+cat sql/drop.sql | docker exec -i ${HYPER_CONTAINER_NAME} psql -U raasveld -p 7484 -h localhost ${HYPER_DATABASE_NAME}
+
+echo Loading data to HyPer...
+cat sql/schema.sql | docker exec -i ${HYPER_CONTAINER_NAME} psql -U raasveld -p 7484 -h localhost ${HYPER_DATABASE_NAME}
+sed "s|PATHVAR|/data|" sql/snb-load.sql | docker exec -i ${HYPER_CONTAINER_NAME} psql -U raasveld -p 7484 -h localhost ${HYPER_DATABASE_NAME}
+echo Done
+
+echo Initializing views and indexes...
+cat sql/schema-constraints.sql | docker exec -i ${HYPER_CONTAINER_NAME} psql -U raasveld -p 7484 -h localhost ${HYPER_DATABASE_NAME}
+echo Done
---
+++ hyp/stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+. hyp/vars.sh
+
+docker stop ${HYPER_CONTAINER_NAME} || echo "No container ${HYPER_CONTAINER_NAME} found"
