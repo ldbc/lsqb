0d67704ad61897908a0e9c7fc5a2600f0d36c240
Gabor Szarnyas <szarnyasg@gmail.com>
2021-03-25 00:01:44 +0100

    Fix Q3b

--- sql/q3b.sql
+++ sql/q3b.sql
@@ -2,9 +2,6 @@ SELECT count(*)
 FROM Message_hasTag_Tag
 JOIN Comment_replyOf_Message
   ON Message_hasTag_Tag.MessageId = Comment_replyOf_Message.ParentMessageId
-JOIN Comment_hasTag_Tag AS cht1
-  ON Comment_replyOf_Message.CommentId = cht1.id
-JOIN Comment_hasTag_Tag AS cht2
-  ON Message_hasTag_Tag.hasTag_Tag = cht2.hasTag_Tag
- AND Comment_replyOf_Message.CommentId = cht2.id
-WHERE Message_hasTag_Tag.hasTag_Tag != cht1.hasTag_Tag;
+JOIN Comment_hasTag_Tag AS cht
+  ON Comment_replyOf_Message.CommentId = cht.id
+WHERE Message_hasTag_Tag.hasTag_Tag != cht.hasTag_Tag;
