51154e8beaadff03eec8b97070df9e196b094e97
Gabor Szarnyas <szarnyasg@gmail.com>
2021-03-27 00:30:25 +0100

    paths, CI

--- rdm/get.sh
+++ rdm/get.sh
@@ -10,7 +10,7 @@ cd scratch
 git clone --branch homomorphic-matching https://github.com/szarnyasg/RapidMatch RapidMatch-homomorphic
 git clone --branch isomorphic-matching  https://github.com/szarnyasg/RapidMatch RapidMatch-isomorphic
 
-cd RapidMatch-homomorphic; mkdir build; cd build; cmake ..; make; cd ..
-cd RapidMatch-isomorphic ; mkdir build; cd build; cmake ..; make; cd ..
+cd RapidMatch-homomorphic; mkdir build; cd build; cmake ..; make; cd ../..
+cd RapidMatch-isomorphic ; mkdir build; cd build; cmake ..; make; cd ../..
 
 cd ..
