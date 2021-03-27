271439fef4647c8ccd3214f9ddcb0adcb34de5a7
Gabor Szarnyas <szarnyasg@gmail.com>
2021-03-27 04:12:42 +0100

    Add MAX flag to RapidMatch to force finding all matches

--- rdm/run.sh
+++ rdm/run.sh
@@ -10,7 +10,7 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
 export RAPIDMATCH_HOMOMORPHIC_DIR=`pwd`/rdm/scratch/RapidMatch-homomorphic/
 export RAPIDMATCH_ISOMORPHIC_DIR=`pwd`/rdm/scratch/RapidMatch-isomorphic/
 export DATA_GRAPH=`pwd`/data/rdm/ldbc-${SF}.graph
-export RAPIDMATCH_PARAMS="-order nd -time_limit 36000 -preprocess true"
+export RAPIDMATCH_PARAMS="-order nd -time_limit 36000 -preprocess true -num MAX"
 
 export QUERY=1; ${RAPIDMATCH_ISOMORPHIC_DIR}/build/matching/RapidMatch.out  ${RAPIDMATCH_PARAMS} -d ${DATA_GRAPH} -q rdm/query_graphs/q${QUERY}.graph | python3 rdm/process.py
 export QUERY=2; ${RAPIDMATCH_ISOMORPHIC_DIR}/build/matching/RapidMatch.out  ${RAPIDMATCH_PARAMS} -d ${DATA_GRAPH} -q rdm/query_graphs/q${QUERY}.graph | python3 rdm/process.py
