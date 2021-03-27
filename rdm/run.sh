c66615faead492260083e1d5380933eea49d8427
Gabor Szarnyas <szarnyasg@gmail.com>
2021-03-27 02:53:40 +0100

    fix py binary name ..

--- rdm/run.sh
+++ rdm/run.sh
@@ -12,8 +12,8 @@ export RAPIDMATCH_ISOMORPHIC_DIR=`pwd`/rdm/scratch/RapidMatch-isomorphic/
 export DATA_GRAPH=`pwd`/data/rdm/ldbc-${SF}.graph
 export RAPIDMATCH_PARAMS="-order nd -time_limit 36000 -preprocess true"
 
-export QUERY=1; ${RAPIDMATCH_ISOMORPHIC_DIR}/build/matching/RapidMatch.out  ${RAPIDMATCH_PARAMS} -d ${DATA_GRAPH} -q rdm/query_graphs/q${QUERY}.graph | python rdm/process.py
-export QUERY=2; ${RAPIDMATCH_ISOMORPHIC_DIR}/build/matching/RapidMatch.out  ${RAPIDMATCH_PARAMS} -d ${DATA_GRAPH} -q rdm/query_graphs/q${QUERY}.graph | python rdm/process.py
-export QUERY=3; ${RAPIDMATCH_HOMOMORPHIC_DIR}/build/matching/RapidMatch.out ${RAPIDMATCH_PARAMS} -d ${DATA_GRAPH} -q rdm/query_graphs/q${QUERY}.graph | python rdm/process.py
-export QUERY=5; ${RAPIDMATCH_ISOMORPHIC_DIR}/build/matching/RapidMatch.out  ${RAPIDMATCH_PARAMS} -d ${DATA_GRAPH} -q rdm/query_graphs/q${QUERY}.graph | python rdm/process.py
-export QUERY=6; ${RAPIDMATCH_ISOMORPHIC_DIR}/build/matching/RapidMatch.out  ${RAPIDMATCH_PARAMS} -d ${DATA_GRAPH} -q rdm/query_graphs/q${QUERY}.graph | python rdm/process.py
+export QUERY=1; ${RAPIDMATCH_ISOMORPHIC_DIR}/build/matching/RapidMatch.out  ${RAPIDMATCH_PARAMS} -d ${DATA_GRAPH} -q rdm/query_graphs/q${QUERY}.graph | python3 rdm/process.py
+export QUERY=2; ${RAPIDMATCH_ISOMORPHIC_DIR}/build/matching/RapidMatch.out  ${RAPIDMATCH_PARAMS} -d ${DATA_GRAPH} -q rdm/query_graphs/q${QUERY}.graph | python3 rdm/process.py
+export QUERY=3; ${RAPIDMATCH_HOMOMORPHIC_DIR}/build/matching/RapidMatch.out ${RAPIDMATCH_PARAMS} -d ${DATA_GRAPH} -q rdm/query_graphs/q${QUERY}.graph | python3 rdm/process.py
+export QUERY=5; ${RAPIDMATCH_ISOMORPHIC_DIR}/build/matching/RapidMatch.out  ${RAPIDMATCH_PARAMS} -d ${DATA_GRAPH} -q rdm/query_graphs/q${QUERY}.graph | python3 rdm/process.py
+export QUERY=6; ${RAPIDMATCH_ISOMORPHIC_DIR}/build/matching/RapidMatch.out  ${RAPIDMATCH_PARAMS} -d ${DATA_GRAPH} -q rdm/query_graphs/q${QUERY}.graph | python3 rdm/process.py
