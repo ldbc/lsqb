22976a50aee4deb7fd9b25d7fd20cb35fb64e39d
Gabor Szarnyas <szarnyasg@gmail.com>
2021-03-30 06:00:00 +0200

    Add PK/FK constraint

--- sql/snb-load.sql
+++ sql/snb-load.sql
@@ -1,3 +1,4 @@
+COPY Person                    FROM 'PATHVAR/Person.csv'                    (DELIMITER '|', HEADER, FORMAT csv);
 COPY Company                   FROM 'PATHVAR/Company.csv'                   (DELIMITER '|', HEADER, FORMAT csv);
 COPY University                FROM 'PATHVAR/University.csv'                (DELIMITER '|', HEADER, FORMAT csv);
 COPY Continent                 FROM 'PATHVAR/Continent.csv'                 (DELIMITER '|', HEADER, FORMAT csv);
@@ -6,7 +7,6 @@ COPY City                      FROM 'PATHVAR/City.csv'                      (DEL
 COPY Forum                     FROM 'PATHVAR/Forum.csv'                     (DELIMITER '|', HEADER, FORMAT csv);
 COPY Comment                   FROM 'PATHVAR/Comment.csv'                   (DELIMITER '|', HEADER, FORMAT csv);
 COPY Post                      FROM 'PATHVAR/Post.csv'                      (DELIMITER '|', HEADER, FORMAT csv);
-COPY Person                    FROM 'PATHVAR/Person.csv'                    (DELIMITER '|', HEADER, FORMAT csv);
 COPY Tag                       FROM 'PATHVAR/Tag.csv'                       (DELIMITER '|', HEADER, FORMAT csv);
 COPY TagClass                  FROM 'PATHVAR/TagClass.csv'                  (DELIMITER '|', HEADER, FORMAT csv);
 COPY Comment_hasTag_Tag        FROM 'PATHVAR/Comment_hasTag_Tag.csv'        (DELIMITER '|', HEADER, FORMAT csv);
