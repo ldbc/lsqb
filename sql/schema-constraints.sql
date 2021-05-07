be89aa9a23cf408e222a99da1110445be408ff00
Gabor Szarnyas <szarnyasg@gmail.com>
2021-05-08 01:58:56 +0200

    Format query

--- sql/schema-constraints.sql
+++ sql/schema-constraints.sql
@@ -1,32 +1,32 @@
 -- create a common table for the message nodes and edges starting from/ending in message nodes.
-create view Message as
-  select id from Comment
-  union all
-  select id from Post;
+CREATE VIEW Message AS
+  SELECT id FROM Comment
+  UNION ALL
+  SELECT id FROM Post;
 
-create view Comment_replyOf_Message as
-  select id AS CommentId, replyOf_Post AS ParentMessageId from Comment
+CREATE VIEW Comment_replyOf_Message AS
+  SELECT id AS CommentId, replyOf_Post AS ParentMessageId FROM Comment
   where replyOf_Post IS NOT NULL
-  union all
-  select id AS CommentId, replyOf_Comment AS ParentMessageId from Comment
+  UNION ALL
+  SELECT id AS CommentId, replyOf_Comment AS ParentMessageId FROM Comment
   where replyOf_Comment IS NOT NULL;
 
-create view Message_hasCreator_Person as
-  select id as MessageId, hasCreator_Person from Comment
-  union all
-  select id as MessageId, hasCreator_Person from Post;
+CREATE VIEW Message_hasCreator_Person AS
+  SELECT id AS MessageId, hasCreator_Person FROM Comment
+  UNION ALL
+  SELECT id AS MessageId, hasCreator_Person FROM Post;
 
-create view Message_hasTag_Tag as
-  select id as MessageId, hasTag_Tag from Comment_hasTag_Tag
-  union all
-  select id as MessageId, hasTag_Tag from Post_hasTag_Tag;
+CREATE VIEW Message_hasTag_Tag AS
+  SELECT id AS MessageId, hasTag_Tag FROM Comment_hasTag_Tag
+  UNION ALL
+  SELECT id AS MessageId, hasTag_Tag FROM Post_hasTag_Tag;
 
-create view Message_isLocatedIn_Country as
-  select id as MessageId, isLocatedIn_Country from Comment
-  union all
-  select id as MessageId, isLocatedIn_Country from Post;
+CREATE VIEW Message_isLocatedIn_Country AS
+  SELECT id AS MessageId, isLocatedIn_Country FROM Comment
+  UNION ALL
+  SELECT id AS MessageId, isLocatedIn_Country FROM Post;
 
-create view Person_likes_Message as
-  select id as PersonId, likes_Comment as MessageId from Person_likes_Comment
-  union all
-  select id as PersonId, likes_Post as MessageId from Person_likes_Post;
+CREATE VIEW Person_likes_Message AS
+  SELECT id AS PersonId, likes_Comment AS MessageId FROM Person_likes_Comment
+  UNION ALL
+  SELECT id AS PersonId, likes_Post AS MessageId FROM Person_likes_Post;
