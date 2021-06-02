0f9937f4ba4845976dafc6297f639085ac3f660b
Gabor Szarnyas <szarnyasg@gmail.com>
2021-06-02 04:40:34 +0200

    Close connection in DuckDB implementation

--- ddb/client.py
+++ ddb/client.py
@@ -51,3 +51,5 @@ with open(f"results/results.csv", "a+") as results_file:
     for i in range(1, 10):
         with open(f"sql/q{i}.sql", "r") as query_file:
             run_query(con, sf, i, query_file.read(), numThreads, results_file)
+
+con.close()
