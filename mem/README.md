31bc59a3a79ab3a0dde5532db4c3c3bf584c82f1
Gabor Szarnyas <szarnyasg@gmail.com>
2021-06-21 18:10:28 +0200

    Bump version to v1.5.0, use Docker Hub image

--- mem/README.md
+++ mem/README.md
@@ -1,3 +1,1 @@
 # Memgraph client
-
-The Docker image is not available on the Docker Hub, so make sure you have downloaded it with `./get.sh`.
--- mem/start.sh
+++ mem/start.sh
@@ -8,14 +8,6 @@ cd ..
 
 . mem/vars.sh
 
-# grab and load docker image if needed
-if docker image inspect memgraph:${MEMGRAPH_VERSION}-community > /dev/null; then
-    echo Docker image found
-else
-    echo Docker image not found, downloading it...
-    curl https://download.memgraph.com/memgraph/v${MEMGRAPH_VERSION}/docker/memgraph-${MEMGRAPH_VERSION}-community-docker.tar.gz | docker load
-fi
-
 docker stop ${MEMGRAPH_CONTAINER} || echo "No container ${MEMGRAPH_CONTAINER} found"
 
 # port changed from 7687 to 27687
@@ -27,7 +19,7 @@ docker run \
     --volume mg_log:/var/log/memgraph:z \
     --volume mg_etc:/etc/memgraph:z \
     --name ${MEMGRAPH_CONTAINER} \
-    memgraph \
+    memgraph/memgraph:${MEMGRAPH_VERSION} \
     --telemetry-enabled=False
 
 sleep 5
