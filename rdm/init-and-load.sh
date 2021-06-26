a5cb8320ecdf4dd23fc2e4b475b254596d96f8c6
Gabor Szarnyas <szarnyasg@gmail.com>
2021-06-26 14:40:04 +0200

    Fix RapidMatch script with NOP-ing

--- rdm/init-and-load.sh
+++ rdm/init-and-load.sh
@@ -5,7 +5,4 @@ set -o pipefail
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-
-./pre-load.sh
-./load.sh
-./post-load.sh
+# do nothing (RapidMatch does not have storage, so loading happens in the same process as running the queries)
