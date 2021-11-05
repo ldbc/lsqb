32cbbea17fb78ca34d0581891712eb6ea70bf753
Gabor Szarnyas <szarnyasg@gmail.com>
2021-11-05 11:48:08 +0100

    Bump version, add connect script

--- neo/start.sh
+++ neo/start.sh
@@ -25,6 +25,7 @@ docker run \
     --volume=${NEO4J_HOME}/import:/var/lib/neo4j/import:z \
     --volume=${NEO4J_HOME}/plugins:/plugins:z \
     --env NEO4J_AUTH=none \
+    --env NEO4JLABS_PLUGINS='["apoc", "graph-data-science"]' \
     --name ${NEO4J_CONTAINER_NAME} \
     neo4j:${NEO4J_VERSION} \
 
--- pos/vars.sh
+++ pos/vars.sh
@@ -1,5 +1,5 @@
 export POSTGRES_HOME=`pwd`/pos/scratch
-export POSTGRES_VERSION=13.4
+export POSTGRES_VERSION=14.0
 export POSTGRES_SCHEMA_DIR=`pwd`/sql
 export POSTGRES_CONTAINER_NAME=lsqb-pos
 export POSTGRES_PASSWORD_POLICY="POSTGRES_PASSWORD=mysecretpassword"
