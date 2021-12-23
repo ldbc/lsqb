dcf5dd8d34fe3d7ac98a5e5680d16f9af857dee9
Gabor Szarnyas <szarnyasg@gmail.com>
2021-12-23 16:51:05 +0100

    Add script to convert Memgraph CSVs and use it in CI

---
+++ mem/convert-csvs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+. mem/vars.sh
+. scripts/import-vars.sh
+
+for FILE in ${IMPORT_DATA_DIR_PROJECTED_FK}/*.csv; do
+    tail -n +2 ${FILE} > ${FILE}-headerless
+done
