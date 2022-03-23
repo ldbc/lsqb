cf92d7199534fa22c471708ac438afb371a2db9d
Gabor Szarnyas <szarnyasg@gmail.com>
2022-03-23 14:36:33 +0100

    Further opt to Q3 SQL

--- sql/q3.sql
+++ sql/q3.sql
@@ -1,14 +1,23 @@
-WITH edge AS (SELECT knows.Person1Id AS p1, knows.Person2Id AS p2, City1.isPartOf_CountryId AS country
-  FROM Person_knows_Person AS knows
-  JOIN Person Person1
-    ON Person1.PersonId = knows.Person1Id
+WITH person_city AS (SELECT Person.PersonId AS PersonId, City.CityId AS CityId
+  FROM Person
+  JOIN City
+    ON City.CityId = Person.isLocatedIn_CityId
+),
+  person_pairs AS (    
+  SELECT knows.Person1Id AS Person1Id, knows.Person2Id AS Person2Id, person_city1.CityId AS City1Id, person_city2.CityId AS City2Id
+    FROM Person_knows_Person AS knows
+    JOIN person_city person_city1
+      ON person_city1.PersonId = knows.Person1Id
+    JOIN person_city person_city2
+      ON person_city2.PersonId = knows.Person2Id
+  ),
+  edge AS (SELECT person_pairs.Person1Id AS p1, person_pairs.Person2Id AS p2, City1.isPartOf_CountryId AS country
+  FROM person_pairs
   JOIN City City1
-    ON City1.CityId = Person1.isLocatedIn_CityId
-  JOIN Person Person2
-    ON Person2.PersonId = knows.Person2Id
+    ON City1.CityId = City1Id
   JOIN City City2
-    ON City2.CityId = Person2.isLocatedIn_CityId
-  WHERE City1.isPartOf_CountryId = City2.isPartOf_CountryId
+    ON City2.CityId = City2Id
+  WHERE City1.isPartOf_CountryId = City2.isPartOf_CountryId    
   ),
   wedges AS (SELECT
     edge1.p1 AS p1,
