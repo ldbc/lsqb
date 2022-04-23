51cb5d371c96d3757a351738c6b70e51015c1dfe
Gabor Szarnyas <szarnyasg@gmail.com>
2022-04-23 16:17:38 +0200

    Bump Umbra version

--- umb/vars.sh
+++ umb/vars.sh
@@ -6,7 +6,7 @@ export COMMON_SQL_DIR=`pwd`/../sql
 export UMBRA_SQL_DIR=`pwd`/sql/
 export UMBRA_SQL_SCRATCH_DIR=`pwd`/scratch/sql
 export UMBRA_CONTAINER_NAME=lsqb-umb
-export UMBRA_VERSION=533b93fd2
+export UMBRA_VERSION=e7415d13f
 export UMBRA_DOCKER_IMAGE=umbra-release:${UMBRA_VERSION}
 
 popd > /dev/null
