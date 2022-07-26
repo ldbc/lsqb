3e4212d91cb1a1eacb2352979e8300c391f59294
Gabor Szarnyas <szarnyasg@gmail.com>
2022-07-26 14:34:52 +0200

    Revert SQL query optimizations

--- sql/q3.sql
+++ sql/q3.sql
@@ -1,37 +1,21 @@
-WITH person_city AS (SELECT Person.PersonId AS PersonId, City.CityId AS CityId
-  FROM Person
-  JOIN City
-    ON City.CityId = Person.isLocatedIn_CityId
-),
-  person_pairs AS (    
-  SELECT knows.Person1Id AS Person1Id, knows.Person2Id AS Person2Id, person_city1.CityId AS City1Id, person_city2.CityId AS City2Id
-    FROM Person_knows_Person AS knows
-    JOIN person_city person_city1
-      ON person_city1.PersonId = knows.Person1Id
-    JOIN person_city person_city2
-      ON person_city2.PersonId = knows.Person2Id
-  ),
-  edge AS (SELECT person_pairs.Person1Id AS p1, person_pairs.Person2Id AS p2, City1.isPartOf_CountryId AS country
-  FROM person_pairs
-  JOIN City City1
-    ON City1.CityId = City1Id
-  JOIN City City2
-    ON City2.CityId = City2Id
-  WHERE City1.isPartOf_CountryId = City2.isPartOf_CountryId    
-  ),
-  wedges AS (SELECT
-    edge1.p1 AS p1,
-    edge2.p2 AS p3,
-    edge2.country AS country
-  FROM edge edge1
-  JOIN edge edge2
-  ON edge1.p2 = edge2.p1
- AND edge1.country = edge2.country
-)
 SELECT count(*)
-FROM wedges
-JOIN edge edge3
-  ON wedges.p3 = edge3.p1
- AND edge3.p2 = wedges.p1
- AND wedges.country = edge3.country
-;
+FROM City AS CityA
+JOIN City AS CityB
+  ON CityB.isPartOf_CountryId = CityA.isPartOf_CountryId
+JOIN City AS CityC
+  ON CityC.isPartOf_CountryId = CityA.isPartOf_CountryId
+JOIN Person AS PersonA
+  ON PersonA.isLocatedIn_CityId = CityA.CityId
+JOIN Person AS PersonB
+  ON PersonB.isLocatedIn_CityId = CityB.CityId
+JOIN Person AS PersonC
+  ON PersonC.isLocatedIn_CityId = CityC.CityId
+JOIN Person_knows_Person AS pkp1
+  ON pkp1.Person1Id = PersonA.PersonId
+ AND pkp1.Person2Id = PersonB.PersonId
+JOIN Person_knows_Person AS pkp2
+  ON pkp2.Person1Id = PersonB.PersonId
+ AND pkp2.Person2Id = PersonC.PersonId
+JOIN Person_knows_Person AS pkp3
+  ON pkp3.Person1Id = PersonC.PersonId
+ AND pkp3.Person2Id = PersonA.PersonId;
--- sql/q9.sql
+++ sql/q9.sql
@@ -1,17 +1,11 @@
-WITH foaf AS (
-SELECT pkp1.Person1Id AS p1id, pkp1.Person2Id AS p2id, pkp2.Person2Id AS p3id
-  FROM Person_knows_Person pkp1
-  JOIN Person_knows_Person pkp2
-    ON pkp1.Person2Id = pkp2.Person1Id
-   AND pkp1.Person1Id != pkp2.Person2Id
+SELECT count(*)
+FROM Person_knows_Person pkp1
+JOIN Person_knows_Person pkp2
+  ON pkp1.Person2Id = pkp2.Person1Id
+ AND pkp1.Person1Id != pkp2.Person2Id
+JOIN Person_hasInterest_Tag
+  ON pkp2.Person2Id = Person_hasInterest_Tag.PersonId
 LEFT JOIN Person_knows_Person pkp3
        ON pkp3.Person1Id = pkp1.Person1Id
       AND pkp3.Person2Id = pkp2.Person2Id
-    WHERE pkp3.Person1Id IS NULL
-)
-SELECT count(*)
-FROM foaf
-JOIN Person_hasInterest_Tag
-  ON Person_hasInterest_Tag.PersonId = foaf.p3id
-;
-
+    WHERE pkp3.Person1Id IS NULL;
