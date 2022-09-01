1777b45ce7e29a3d857aac1de2ee7a4bb1486b79
Gabor Szarnyas <szarnyasg@gmail.com>
2022-09-01 13:43:04 +0200

    cleanup load script output

--- ddb/load.sh
+++ ddb/load.sh
@@ -9,13 +9,5 @@ cd ..
 . ddb/vars.sh
 . scripts/import-vars.sh
 
-rm -rf ${DUCKDB_DIR}/ldbc.duckdb*
-
-echo -n "Loading data to DuckDB... "
-cat sql/schema.sql | ${DUCKDB_BINARY} ${DUCKDB_DIR}/ldbc.duckdb
-sed "s|PATHVAR|${IMPORT_DATA_DIR_MERGED_FK}|" sql/snb-load.sql
-echo Done
-
-echo -n "Initializing views and indexes... "
-cat sql/views.sql | ${DUCKDB_BINARY} ${DUCKDB_DIR}/ldbc.duckdb
-echo Done
+echo "Running DuckDB experiment at SF${SF}"
+# no op
