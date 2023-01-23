57344d4fdae2bd5051ce7988207a935d408c6692
Gabor Szarnyas <szarnyasg@gmail.com>
2023-01-23 13:17:17 +0100

    Fix projected FK download script

--- scripts/download-projected-fk-data-sets.sh
+++ scripts/download-projected-fk-data-sets.sh
@@ -9,5 +9,5 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd ../data
 
 for SF in 0.1 0.3 1 3 10 30 100 300 1000; do
-    curl --silent --fail https://repository.surfsara.nl/datasets/cwi/lsqb/files/lsqb-merged/social-network-sf${SF}-projected-fk.tar.zst | tar -xv --use-compress-program=unzstd
+    curl --silent --fail https://repository.surfsara.nl/datasets/cwi/lsqb/files/lsqb-projected/social-network-sf${SF}-projected-fk.tar.zst | tar -xv --use-compress-program=unzstd
 done
