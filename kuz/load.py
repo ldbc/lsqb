db9e3cf79321dc9bda6f082a86a19407891eac2b
dtenwolde <dljtw98@gmail.com>
2023-01-25 14:49:47 +0100

    Separated node and edge files

--- kuz/load.py
+++ kuz/load.py
@@ -21,13 +21,19 @@ def load_schema(conn):
 def load_lsqb_dataset(conn, sf):
     data_path = f'data/social-network-sf{sf}-projected-fk'
 
-    lsqb_files = [f for f in listdir(data_path) if isfile(join(data_path, f)) and ".csv" in f]
+    lsqb_node_files = [f for f in listdir(data_path) if isfile(join(data_path, f)) and not "_" in f and ".csv" in f]
+    lsqb_edge_files = [f for f in listdir(data_path) if isfile(join(data_path, f)) and "_" in f and ".csv" in f]
 
     load_schema(conn)
 
-    for lsqb_file in lsqb_files:
+    for lsqb_file in lsqb_node_files:
         logging.debug(f"Loading {lsqb_file}")
         conn.execute(f"""COPY {lsqb_file[:-4]} from '{join(data_path, lsqb_file)}' (HEADER=True, DELIM='|')""")
+
+    for lsqb_file in lsqb_edge_files:
+        logging.debug(f"Loading {lsqb_file}")
+        conn.execute(f"""COPY {lsqb_file[:-4]} from '{join(data_path, lsqb_file)}' (HEADER=True, DELIM='|')""")
+
 
     logging.info("Loaded data files")
 
@@ -39,7 +45,10 @@ def main():
     shutil.rmtree(database_file_location, ignore_errors=True)
 
     db = kuzu.database('kuz/scratch/lsqb-database')
+
     conn = kuzu.connection(db)
+
+
     logging.info("Successfully connected")
 
     load_lsqb_dataset(conn, SF)
