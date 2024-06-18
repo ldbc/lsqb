47a53c7b89abbf488269b96377f77b162ca0dcb7
Gabor Szarnyas <szarnyasg@gmail.com>
2024-06-18 17:02:28 +0200

    restore neo4j script

--- neo/client.py
+++ neo/client.py
@@ -1,33 +1,43 @@
+from neo4j import GraphDatabase, unit_of_work
 import time
 import sys
-import kuzu
 
-def run_query(conn, system_variant, sf, query_id, query_spec, results_file):
+@unit_of_work(timeout=3000)
+def query_fun(tx, query):
+    result = tx.run(query)
+    return result.single()
+
+def run_query(session, system_variant, sf, query_id, query_spec, results_file):
     start = time.time()
-    results = conn.execute(query_spec)
-    if results.hasNext():
-        result = results.getNext()[0]
-
+    # turn on the parallel runtime for the Enterprise edition
+    if system_variant == "enterprise" and query_id != 9:
+        query_spec = f"CYPHER runtime=parallel {query_spec}"
+    result = session.read_transaction(query_fun, query_spec)
     end = time.time()
     duration = end - start
-    results_file.write(f"KuzuDB\t{system_variant}\t{sf}\t{query_id}\t{duration:.4f}\t{result[0]}\n")
+    results_file.write(f"Neo4j\t{system_variant}\t{sf}\t{query_id}\t{duration:.4f}\t{result[0]}\n")
     results_file.flush()
     return (duration, result)
 
+if len(sys.argv) < 2:
+    print("Usage: client.py sf <system variant>")
+    print("Where sf is the scale factor and the system variant is the edition used (e.g. community, enterprise)")
+    exit(1)
+else:
+    sf = sys.argv[1]
 
-def main():
-    if len(sys.argv) < 1:
-        print("Usage: client.py sf")
-        print("Where sf is the scale factor)")
-        exit(1)
-    else:
-        sf = sys.argv[1]
+if len(sys.argv) > 2:
+    system_variant = sys.argv[2]
+else:
+    system_variant = ""
 
-    db = kuzu.database('./scratch/lsqb-database')
-    conn = kuzu.connection(db)
+driver = GraphDatabase.driver("bolt://localhost:7687")
+session = driver.session()
 
-    with open(f"results/results.csv", "a+") as results_file:
-        #for i in range(1, 10):
-        for i in [1, 2, 3, 4, 5, 6]:
-            with open(f"kuz/q{i}.cypher", "r") as query_file:
-                run_query(conn, "", sf, i, query_file.read(), results_file)
+with open(f"results/results.csv", "a+") as results_file:
+    for i in range(1, 10):
+        with open(f"cypher/q{i}.cypher", "r") as query_file:
+            run_query(session, system_variant, sf, i, query_file.read(), results_file)
+
+session.close()
+driver.close()
