ff5cccf541c0fc0242f696aaa54b26fcac06aca5
Gabor Szarnyas <szarnyasg@gmail.com>
2024-06-18 17:01:47 +0200

    bump neo4j version

--- neo/vars.sh
+++ neo/vars.sh
@@ -1,5 +1,5 @@
 export NEO4J_HOME=`pwd`/neo/scratch
 export NEO4J_DATA_DIR=`pwd`/neo/scratch/data
-export NEO4J_VERSION=5.7.0
+export NEO4J_VERSION=5.20.0
 export NEO4J_ENV_VARS=""
 export NEO4J_CONTAINER_NAME=lsqb-neo
