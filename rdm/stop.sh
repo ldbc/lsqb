c762d9e907974a358315bf9630b54781aa73444d
Gabor Szarnyas <szarnyasg@gmail.com>
2024-06-18 17:06:39 +0200

    remove rapidmatch

--- rdm/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eu
-set -o pipefail
-
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# do nothing (RapidMatch does not need a separate stopping script)
