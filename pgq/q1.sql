05834a396090ac730dc7fa03954d0338def50aac
Gabor Szarnyas <szarnyasg@gmail.com>
2025-02-27 11:05:36 +0100

    Format queries

--- pgq/q1.sql
+++ pgq/q1.sql
@@ -1,3 +1,3 @@
 SELECT count(*) FROM GRAPH_TABLE (lsqb
     MATCH (c:Country)<-[ipo:City_isPartOf_Country]-(c1:City)<-[ilo:Person_isLocatedIn]-(p:Person)<-[hm:hasMember]-(f:Forum)-[co:containerOf]->(po:Post)<-[ro:replyOf_Post]-(c2:Comment)-[ht:Comment_HasTag]->(t:Tag)-[ht2:hasType]->(tc:TagClass)
-)
+);
--- pgq/q6.sql
+++ pgq/q6.sql
@@ -2,4 +2,4 @@ SELECT count(*)
 FROM GRAPH_TABLE (lsqb
     MATCH (person1:Person)-[k:knows]-(person2:Person)-[k2:knows]-(person3:Person)-[hi:hasInterest]->(T:Tag)
     WHERE person1.id <> person3.id
-)
+);
