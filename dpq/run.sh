7229e2b8aed77990d008dfe447c83fa63a7d5830
dtenwolde <dljtw98@gmail.com>
2025-02-27 10:16:24 +0100

    Fix var name and set run to 5

--- dpq/run.sh
+++ dpq/run.sh
@@ -8,7 +8,7 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ../.venv/bin/activate
 
 SFS="1 3 10"
-RUNS=1
+RUNS=5
 NUM_THREADS=${1:-`nproc`}
 
 export SF
@@ -16,8 +16,8 @@ for SF in ${SFS}; do
     echo Running benchmark on scale factor ${SF}
     for RUN in $(seq 1 ${RUNS}); do
       echo "Run ${RUN} of ${RUNS}"
-      python load.py ${SF} ${THREADS}
-      python client.py ${SF} ${THREADS}
+      python load.py ${SF} ${NUM_THREADS}
+      python client.py ${SF} ${NUM_THREADS}
     done
 done
 deactivate
