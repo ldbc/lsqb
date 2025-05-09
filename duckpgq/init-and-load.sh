fef1b88299327553dc4aec0775c03c185e49bf0b
Gabor Szarnyas <szarnyasg@gmail.com>
2025-05-09 09:46:03 +0200

    DuckPGQ: Add to CI, cleanup scripts

---
+++ duckpgq/init-and-load.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
+
+. scripts/import-vars.sh
+
+NUM_THREADS=${1:-`nproc`}
+
+cd duckpgq
+python load.py ${SF} ${NUM_THREADS}
--- duckpgq/run.sh
+++ duckpgq/run.sh
@@ -4,19 +4,11 @@ set -eu
 set -o pipefail
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd ..
 
-SFS="1"
-RUNS=1
+. scripts/import-vars.sh
+
 NUM_THREADS=${1:-`nproc`}
 
-export SF
-for SF in ${SFS}; do
-    rm -rf scratch/lsqb-${SF}.duckdb*
-
-    echo Running benchmark on scale factor ${SF}
-    for RUN in $(seq 1 ${RUNS}); do
-      echo "Run ${RUN} of ${RUNS}"
-      python load.py ${SF} ${NUM_THREADS}
-      python client.py ${SF} ${NUM_THREADS}
-    done
-done
+cd duckpgq
+python client.py ${SF} ${NUM_THREADS}
